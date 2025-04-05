506511dbe7ed9ebc0b16a120de25779da2fa5b2f
thotho19 <thoalfokar.almuathen1@gmail.com>
2025-04-05 15:00:05 +0300

    fix lusitana font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,6 +2,7 @@ import { Inter, Lusitana } from "next/font/google";
 
 export const inter = Inter({ subsets: ['latin'] })
 export const lusitana = Lusitana({
+    subsets: ['latin'],
     weight: ['400'],
     variable: '--font-lusitana'
 });
